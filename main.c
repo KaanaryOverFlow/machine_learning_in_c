cb88f6bdd58791d622db6b482167f66a99d908f6
Adınız <siz@e-posta.com>
2024-08-29 19:40:32 +0300

    backprop added
    Signed-off-by: Adınız <siz@e-posta.com>

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ double rand_double(void) {
 }
 
 Mat mat_alloc(size_t rows, size_t cols) {
-	double h = 2;
+	double h = 1;
 	double l = 0;
 	Mat m;
 	m.rows = rows;
@@ -32,7 +32,7 @@ Mat mat_alloc(size_t rows, size_t cols) {
 	srand(time(NULL));
 	FOR(m.rows) {
 		FORR(m.cols) {
-			MAT_AT(m, i, ii) = rand_double() * (h - l) + l;
+			MAT_AT(m, i, ii) = rand_double() * (h - l) + l + 1e-1;
 		}
 	}
 
@@ -86,7 +86,7 @@ void mat_dot(Mat dest, Mat a, Mat b) {
 }
 
 double sigmoidf(double x) {
-	return 1 / (1 + expf(-x));
+	return 1 / (1 + exp(-x));
 }
 
 void mat_sig(Mat m) {
@@ -135,6 +135,7 @@ typedef struct {
 
 	Mat *dw;
 	Mat *db;
+	Mat *gas;
 } NN;
 
 #define ARRAY_LEN(x) sizeof(x)/sizeof(x[0])
@@ -148,8 +149,11 @@ NN nn_alloc(size_t *arch, size_t count) {
 	
 	nn.dw = malloc(sizeof(Mat) * nn.count);
 	nn.db = malloc(sizeof(Mat) * nn.count);
+	nn.gas = malloc(sizeof(Mat) * count);
 
 	nn.as[0] = mat_alloc(1, arch[0]);
+	nn.gas[0] = mat_alloc(1, arch[0]);
+	mat_fill(nn.gas[0], 0);
 
 	FOR(nn.count) {
 		nn.ws[i] = mat_alloc(nn.as[i].cols, arch[i+1]);
@@ -158,6 +162,12 @@ NN nn_alloc(size_t *arch, size_t count) {
 		
 		nn.dw[i] = mat_alloc(nn.as[i].cols, arch[i+1]);
 		nn.db[i] = mat_alloc(1 , arch[i+1]);
+		nn.gas[i+1] = mat_alloc(1 , arch[i+1]);
+
+
+		mat_fill(nn.dw[i], 0);
+		mat_fill(nn.db[i], 0);
+		mat_fill(nn.gas[i + 1], 0);
 		
 	}
 
@@ -242,9 +252,174 @@ void nn_learn(NN nn, double eps, double rate, Mat ti, Mat to) {
 	}
 }
 
+void commit_diff(NN nn, double rate) {
+	FOR(nn.count) {
+		for (size_t row = 0; row < nn.ws[i].rows; row++) {
+			for (size_t col = 0; col < nn.ws[i].cols; col++) {
+				MAT_AT(nn.ws[i], row, col) -= rate *  MAT_AT(nn.dw[i], row, col);
+			}
+		}
+		
+		for (size_t row = 0; row < nn.bs[i].rows; row++) {
+			for (size_t col = 0; col < nn.bs[i].cols; col++) {
+				MAT_AT(nn.bs[i], row, col) -= rate *  MAT_AT(nn.db[i], row, col);
+			}
+		}
+	}
+}
+
+
+void nn_backprop(NN nn, double rate, Mat input, Mat output) {
+	
+	mat_fill(nn.gas[0], 0);
+	
+	FOR(nn.count) {
+		mat_fill(nn.dw[i], 0);
+		mat_fill(nn.db[i], 0);
+		mat_fill(nn.gas[i + 1], 0);
+	}
+	
+	FOR(input.rows) {
+
+		mat_copy(nn_in(nn), mat_row(input, i));
+		nn_ileri(nn);
+		
+		FORR(nn.count) {
+			mat_fill(nn.gas[ii], 0);
+
+		}
+		
+		FORR(output.cols) {
+			MAT_AT(nn.gas[nn.count], 0, ii) = MAT_AT(nn_out(nn), 0, ii) - MAT_AT(output, i, ii);
+		}
+		for (size_t layer = nn.count; layer > 0; layer--) {
+			for (size_t col = 0; col < nn.as[layer].cols; col++) {
+				double a = MAT_AT(nn.as[layer], 0, col);
+				double da = MAT_AT(nn.gas[layer], 0, col);
+				MAT_AT(nn.db[layer - 1], 0, col) += 2 * da * a * (1 - a);
+				for (size_t row = 0; row < nn.as[layer - 1].cols; row++) {
+					double pa = MAT_AT(nn.as[layer - 1], row, col);
+					double w = MAT_AT(nn.ws[layer - 1], 0, row);
+					MAT_AT(nn.dw[layer - 1], row, col) += 2 * da * a * (1 - a) * pa;
+					MAT_AT(nn.gas[layer - 1], 0, row) += 2 * da * a * (1 - a) * w;
+
+				}
+
+
+			}
+		}
+	}
+	
+
+	FOR(nn.count) {
+		for (size_t row = 0; row < nn.ws[i].rows; row++) {
+			for (size_t col = 0; col < nn.ws[i].cols; col++) {
+				MAT_AT(nn.dw[i], row, col) /= input.rows;
+			}
+		}
+		
+		for (size_t row = 0; row < nn.bs[i].rows; row++) {
+			for (size_t col = 0; col < nn.bs[i].cols; col++) {
+				MAT_AT(nn.db[i], row, col) /= input.rows;
+			}
+		}
+	}
+	
+	commit_diff(nn, rate);
+
+	
+}
+
+
+
+void my_learn(NN nn, double rate, Mat input, Mat output) {
+	
+	mat_fill(nn.gas[0], 0);
+	
+	FOR(nn.count) {
+		mat_fill(nn.dw[i], 0);
+		mat_fill(nn.db[i], 0);
+		mat_fill(nn.gas[i + 1], 0);
+	}
+	
+	FOR(input.rows) {
+
+		mat_copy(nn_in(nn), mat_row(input, i));
+		nn_ileri(nn);
+	
+		mat_fill(nn.gas[0], 0);
+		
+		FORR(nn.count) {
+			mat_fill(nn.gas[ii], 0);
+
+		}
+
+		
+		FORR(output.cols) {
+			MAT_AT(nn.gas[nn.count], 0, ii) = MAT_AT(nn_out(nn), 0, ii) - MAT_AT(output, i, ii);
+		}
+		
+		for (size_t layer = nn.count; layer > 0; layer--) {
+			for (size_t col = 0; col < nn.as[layer].cols; col++) { // çıktıda ne kadar nöron varsa o kadar dön
+				double delta_predict_cost = 2 * MAT_AT(nn.gas[layer], 0, col);
+				double delta_sigmoid = MAT_AT(nn.as[layer], 0, col) * (1 - MAT_AT(nn.as[layer], 0, col));
+				// phex(delta_predict_cost);
+				// phex(delta_sigmoid);
+				if (!delta_predict_cost) {
+					note("delta_predict_cost sıfır 2 * %lf", MAT_AT(nn.gas[nn.count], 0, col));
+					mat_p(nn.gas[nn.count]);
+					getchar();
+				}
+				if (!delta_sigmoid) {
+					note("delta_sigmoid sıfır");
+					getchar();
+				}
+				MAT_AT(nn.db[layer - 1], 0, col) += delta_predict_cost * delta_sigmoid;
+				// phex(MAT_AT(nn.db[layer - 1], 0, col));
+				for (size_t input_count = 0; input_count < nn.as[layer - 1].cols; input_count++) { // mevcut nöron kaç tane input almışsa o kadar dön
+					double in = MAT_AT(nn.as[layer - 1], 0, input_count);
+					double w = MAT_AT(nn.ws[layer - 1], input_count, col);
+					
+					MAT_AT(nn.dw[layer - 1], input_count, col) += delta_predict_cost * delta_sigmoid * in;
+					MAT_AT(nn.gas[layer - 1], 0, input_count) += delta_predict_cost * delta_sigmoid * w;
+				
+				}
+
+			}
+		}
+		
+	}
+
+	FOR(nn.count) {
+		for (size_t row = 0; row < nn.ws[i].rows; row++) {
+			for (size_t col = 0; col < nn.ws[i].rows; col++) {
+				// MAT_AT(nn.dw[i], row, col) /= input.rows;
+			}
+		}
+		for (size_t row = 0; row < nn.bs[i].rows; row++) {
+			for (size_t col = 0; col < nn.bs[i].rows; col++) {
+				// MAT_AT(nn.db[i], row, col) /= input.rows;
+			}
+		}
+	}
+
+	commit_diff(nn, rate);
+
+}
+
+void print_delta(NN nn) {
+	mat_p(nn.gas[0]);
+	
+	FOR(nn.count) {
+		mat_p(nn.dw[i]);
+		mat_p(nn.db[i]);
+		mat_p(nn.gas[i + 1]);
+	}
+}
+
 void sec_main(char *param) {
 	
-	size_t arch[] = {2, 5, 4, 1};
+	size_t arch[] = {2, 2, 1};
 	NN nn = nn_alloc(arch, ARRAY_LEN(arch));
 
 
@@ -265,9 +440,11 @@ void sec_main(char *param) {
 	double eps = 1e-1;
 	double rate = 1e-1;
 
+	do {
 	note("cost : %lf", nn_cost(nn, ti, to));
-	FOR(100 * 1000)
-		nn_learn(nn, eps, rate, ti, to);
+	FOR(3000)
+	// nn_learn(nn, eps, rate, ti, to);
+		my_learn(nn, rate, ti, to);
 	note("cost : %lf", nn_cost(nn, ti, to));
 	
 	FOR(2) {
@@ -278,6 +455,7 @@ void sec_main(char *param) {
 			note("%zu ^ %zu = %lf",i, ii, MAT_AT(nn_out(nn), 0, 0));
 		}
 	}
+	} while(0);
 }
 
 
